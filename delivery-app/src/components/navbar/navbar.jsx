2ee2900724aa0ebbdb33f7a1a441c6e826b4557a
Aryankpoor <Kapooraryan0419@gmail.com>
2024-06-18 18:32:14 +0530

    changed assets location and added navbar to the project

--- delivery-app/src/components/navbar/navbar.jsx
+++ delivery-app/src/components/navbar/navbar.jsx
@@ -3,7 +3,20 @@ import './navbar.css'
 const navbar = () => {
   return (
     <div className='navbar'>
-      <img src={AuthenticatorAssertionResponse.logo} alt="" className="logo" />
+      <img src="logo.png" alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile App</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src="/assets/search_icon.png" alt ="" />
+        <div className="navbar-search-icon">
+          <img src="/assets/basket_icon.png" alt="basket icon"></img>
+          <div className="dot"></div>
+        </div>
+      </div>
     </div>
   )
 }
